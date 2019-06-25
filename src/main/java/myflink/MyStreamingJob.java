af502c7a04c30589844676344fe8a21c62d5383c
douming <douming@corp.netease.com>
2019-06-25 14:52:17 +0800

    add git ignore.

---
+++ src/main/java/myflink/MyStreamingJob.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package myflink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import java.util.Random;
+
+/**
+ * Skeleton for a Flink Streaming Job.
+ *
+ * <p>For a tutorial how to write a Flink streaming application, check the
+ * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class MyStreamingJob {
+
+	public static void main(String[] args) throws Exception {
+		// set up the streaming execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+
+		/*
+		 * Here, you can start creating your execution plan for Flink.
+		 *
+		 * Start with getting some data from the environment, like
+		 * 	env.readTextFile(textPath);
+		 *
+		 * then, transform the resulting DataStream<String> using operations
+		 * like
+		 * 	.filter()
+		 * 	.flatMap()
+		 * 	.join()
+		 * 	.coGroup()
+		 *
+		 * and many more.
+		 * Have a look at the programming guide for the Java API:
+		 *
+		 * http://flink.apache.org/docs/latest/apis/streaming/index.html
+		 *
+		 */
+
+		DataStream<String> text = env.socketTextStream("106.14.98.121",8888);
+
+        DataStream<Tuple4<String,String, Integer,Integer>> wordStream =
+        text.flatMap(new FlatMapFunction<String, Tuple4<String,String, Integer,Integer>>() {
+            @Override
+            public void flatMap(String originalValue, Collector<Tuple4<String,String, Integer,Integer>> collector) throws Exception {
+                for(String word: originalValue.split("\\s")){
+                    int a = new Random().nextInt();
+                    String tempStr = word+a;
+                    collector.collect(Tuple4.of(tempStr,word,1, a));
+                }
+            }
+        });
+
+        wordStream.print().setParallelism(1);
+
+       DataStream<Tuple4<String,String, Integer,Integer>> windowCounts =    wordStream.keyBy(1).timeWindow(Time.seconds(10)).sum(2);
+
+
+
+
+
+        windowCounts.print().setParallelism(1);
+
+        // execute program
+		env.execute("Flink Streaming Java API Skeleton");
+	}
+}
